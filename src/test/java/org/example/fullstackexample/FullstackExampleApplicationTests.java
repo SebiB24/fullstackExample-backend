f6971b02f5072a0bf485c6d419bd8a8270f8eddd
SebiB24 <sebycool242@gmail.com>
2025-05-15 19:37:16 +0300

    Initial commit

---
+++ src/test/java/org/example/fullstackexample/FullstackExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fullstackexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
